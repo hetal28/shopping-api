151ad58a8b8d247e22a6252c30f9112d59feae40
hetal28 <49924773+hetal28@users.noreply.github.com>
2025-03-06 09:42:31 -0500

    Add files via upload

---
+++ notification-service/src/test/java/com/tech/microservices/notification/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tech.microservices.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
